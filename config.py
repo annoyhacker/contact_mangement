4ea9db7cea5eae15d63729ad3b5a42e239a2db17
annoyhacker <annoyhacker190@gmail.com>
2025-04-02 23:53:20 +0530

    deploy on render

--- config.py
+++ config.py
@@ -3,8 +3,7 @@ import os
 class Config:
     SECRET_KEY = 'Tjd1fB1cQATaY9m2WAwfFxMuCHeCzw1PQ464wFO_i40'
 
-    DATABASE_URL= 'postgresql://root:rL0HhK3RVJZdxDr16YNhoJbN4jVgoYyJ@dpg-cvmnepe3jp1c73dv6ab0-a/test_contact_manger'
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:ADMIN@localhost/contact_db'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'ZQeDOsFytKpJPlwUmapCcmVRhInyePv7AC0x2tvIUdKx4BhoPmIL6NB12CdSgu0EhdJN_yNYMloyTjCzbd6jZQ'
